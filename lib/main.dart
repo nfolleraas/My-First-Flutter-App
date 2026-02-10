c51e17b12621142c104068057de944aaa210e722
Nikolaj Folleraas <nfolleraas@gmail.com>
2026-02-10 15:22:47 +0100

    Implement dynamic page switching in MyHomePage based on selectedIndex

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No widget for $selectedIndex');
+    }
+
     return Scaffold(
       body: Row(
         children: [
@@ -86,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
